f4d75fd6dbe6717825d16da60416f6304ef31786
Janak Agrawal <janak2@users.noreply.github.com>
2017-11-06 03:50:14 -0500

    Create letter_combinations_of_phone_numbers.cpp
    You did a bottom up recurssion but top down recurssion also possible.
    combine(digits,0,m,string);

---
+++ letter_combinations_of_phone_numbers.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    vector<string> combine(string digits, int i, map<char,string> m)
+    {
+        vector<string> ans;
+        if(i==digits.length()-1)
+        {
+            for(char c: m[digits[i]]) ans.push_back(string(1,c));
+        }
+        else
+        {
+        vector<string> s = combine(digits,i+1,m);
+        for(char c: m[digits[i]])
+        {
+            for(string t: s)
+            {
+                ans.push_back(c+t);
+            }
+        }
+        }
+        return ans;
+    }
+    vector<string> letterCombinations(string digits) {
+        map<char,string> m;
+        m['2'] = "abc";
+        m['3'] = "def";
+        m['4'] = "ghi";
+        m['5'] = "jkl";
+        m['6'] = "mno";
+        m['7'] = "pqrs";
+        m['8'] = "tuv";
+        m['9'] = "wxyz";
+        if(digits.length()==0)return vector<string>(0);
+        return combine(digits,0,m);
+    }
+};
